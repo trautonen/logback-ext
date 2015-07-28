12fca385213503a5ed951dc093acbaf04664d21e
trautonen <trautonen@github.com>
2015-07-28 13:51:45 +0300

    Async dynamodb appender.

---
+++ logback-ext-dynamodb-appender/src/main/java/org/eluder/logback/ext/dynamodb/appender/AsyncDynamoDbAppender.java
@@ -0,0 +1,91 @@
+package org.eluder.logback.ext.dynamodb.appender;
+
+import ch.qos.logback.classic.AsyncAppender;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Context;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.encoder.Encoder;
+
+import java.nio.charset.Charset;
+
+public class AsyncDynamoDbAppender extends AsyncAppender {
+
+    private final DynamoDbAppender appender;
+
+    public AsyncDynamoDbAppender() {
+        DynamoDbAppender appender = new DynamoDbAppender();
+        appender.setAsyncParent(true);
+        addAppender(appender);
+        this.appender = appender;
+    }
+
+    public void setAccessKey(String accessKey) {
+        appender.setAccessKey(accessKey);
+    }
+
+    public void setSecretKey(String secretKey) {
+        appender.setSecretKey(secretKey);
+    }
+
+    public void setMaxPayloadSize(int maxPayloadSize) {
+        appender.setMaxPayloadSize(maxPayloadSize);
+    }
+
+    public void setRegion(String region) {
+        appender.setRegion(region);
+    }
+
+    public void setTable(String table) {
+        appender.setTable(table);
+    }
+
+    public void setPrimaryKey(String primaryKey) {
+        appender.setPrimaryKey(primaryKey);
+    }
+
+    public final void setThreadPoolSize(int threadPoolSize) {
+        appender.setThreadPoolSize(threadPoolSize);
+    }
+
+    @Override
+    public final void setMaxFlushTime(int maxFlushTime) {
+        appender.setMaxFlushTime(maxFlushTime);
+        // add an extra 100 millis to wait for the internal event queue handling
+        super.setMaxFlushTime(maxFlushTime + 100);
+    }
+
+    public void setCharset(Charset charset) {
+        appender.setCharset(charset);
+    }
+
+    public void setEncoder(Encoder<ILoggingEvent> encoder) {
+        appender.setEncoder(encoder);
+    }
+
+    public void setLayout(Layout<ILoggingEvent> layout) {
+        appender.setLayout(layout);
+    }
+
+    public void setBinary(boolean binary) {
+        appender.setBinary(binary);
+    }
+
+    @Override
+    public void setName(String name) {
+        appender.setName(name);
+        super.setName(name);
+    }
+
+    @Override
+    public void setContext(Context context) {
+        appender.setContext(context);
+        super.setContext(context);
+    }
+
+    @Override
+    public void start() {
+        appender.start();
+        super.start();
+    }
+
+}
